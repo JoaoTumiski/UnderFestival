fc6487de4d3e91c8fafaa88e5b864a06af86518e
JoãoTumiski <joaotumiski@gmail.com>
2023-05-12 01:41:38 -0300

    termino tela de cotas e começo da tela de artistas

--- app/src/main/java/com/example/underfestival/Fragments/Cotas.kt
+++ app/src/main/java/com/example/underfestival/Fragments/Cotas.kt
@@ -5,9 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.underfestival.Adapter.ViewPagerAdapter
-import com.example.underfestival.R
-import com.example.underfestival.databinding.FragmentArtistasBinding
 import com.example.underfestival.databinding.FragmentCotasBinding
 
 
